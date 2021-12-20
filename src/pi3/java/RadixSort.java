7cdf7dc69a0e9289af8c4d54763939a6b205eaf1
VanessaGomesOliveira <vanessa.goliveira@mercadolivre.com>
2021-12-20 18:06:33 -0300

    exercicio pi3

---
+++ src/pi3/java/RadixSort.java
@@ -0,0 +1,45 @@
+package pi3.java;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public static void radixSort(int iArr[]) {
+
+    }
+
+    public static void ordenaLista(int iArr[]) {
+        Arrays.sort(iArr);
+        for (int i : iArr) {
+            System.out.println(i + "");
+        }
+    }
+
+    public static String[] parseString(int iArr[]) {
+        String arrStr[];
+        for (int i : iArr) {
+            System.out.println(i + "");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int iArr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511,8 };
+        radixSort(iArr);
+        for (int i : iArr) {
+            System.out.println(i + "");
+        }
+
+        System.out.println("\n\n\n");
+
+        ordenaLista(iArr);
+
+    }
+}
+
+/*
+
+
+
+
+*/
